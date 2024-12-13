03731c5f6cb3dbe87e0fc055e8fb3bf38c146899
kebora <125854657+kebora@users.noreply.github.com>
2024-12-13 07:01:55 +0300

    fix: init repository

--- lib/bitvcs.dart
+++ lib/bitvcs.dart
@@ -7,13 +7,33 @@ void initRepository() {
     print('Repository already initialized.');
   } else {
     bitDir.createSync();
+    //
     Directory('.bitvcs/objects').createSync();
+    Directory('.bitvcs/refs').createSync();
+    // main branch
+    File('.bitvcs/HEAD').writeAsStringSync('ref: refs/heads/main');
     File('.bitvcs/index').createSync();
-    File('.bitvcs/commits').createSync();
+    File('.bitvcs/ignore').createSync();
     print('Initialized empty repository in ${Directory.current.path}/.bitvcs');
   }
 }
 //
 void addFiles(List<String> files){
+    if (files.isEmpty) {
+    print('No files specified to add.');
+    return;
+  }
 
+  final indexFile = File('.bitvcs/index');
+  if (!indexFile.existsSync()) {
+    print('Error: Repository not initialized. Run "bitvcs init".');
+    return;
+  }
+
+  final currentIndex = indexFile.readAsStringSync().split('\n').where((line) => line.isNotEmpty).toList();
+  currentIndex.addAll(files);
+  indexFile.writeAsStringSync('${currentIndex.join('\n')}\n');
+  print('Added files: ${files.join(', ')}');
 }
+
+//
